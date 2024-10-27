55b267e2732655103261fd3d31073b494cdcf9e6
Kamish-69 <kamish7569@gmail.com>
2024-10-27 18:02:02 +0530

    .

--- Chapter 4.py
+++ Chapter 4.py
@@ -15,3 +15,17 @@ for performer in performers:
     print(f"\tI can't wait to see the next part, {performer.title()}.")
 print()
 print("All of you did amazing, guys! Keep it up.")
+
+## TIY Page 56
+
+# TIY 4-1
+pizzas = ['pizza1','pizza2','pizza3']
+for pizza in pizzas:
+    print(f"I like {pizza}.")
+print(f"I really love pizza.")
+
+# TIY 4-2
+birds = ['parrot','hummingbird','crow']
+for bird in birds:
+    print(f"{bird.title()}s can fly.")
+print("All these animals are birds!")
