8aa24fab9dcd6809add0d54d711d2dd62ea9b9e4
Kamish-69 <kamish7569@gmail.com>
2024-10-31 19:38:03 +0530

    .

--- Chapter 5.py
+++ Chapter 5.py
@@ -16,3 +16,23 @@ print(f"\nWaiter - 'Here is your ordered pizza with {served_topping}.'")
 if served_topping.lower() != requested_topping.lower():
     print(f"Customer - 'But I wanted {requested_topping.lower()} topping.'")
 
+# Using 'and' to check multiple conditions + Chained Comparisons
+Jack = 14
+david = 19
+if 13 <= Jack <= 19 and 13 <= david <= 19:
+    print(f"\n\nJack and David are teenagers.")
+
+# Elif
+Jack = 12
+david = 15
+
+if 13 <= Jack <= 19 and 13 <= david <= 19:
+    print(f"\n\nJack and David are teenagers.")
+elif 13 <= Jack <=19:
+    print("\nJack is a teenager.")
+elif 13 <= david <= 19:
+    print("\nDavid is a teenager.")
+else:
+    print("Neither Jack or David is a teenager")
+
+
