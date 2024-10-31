8ba299ca8bb3dde25b8ac7382336a3cad77ae8c9
Kamish-69 <kamish7569@gmail.com>
2024-10-31 18:40:48 +0530

    .

---
+++ Chapter 5.py
@@ -0,0 +1,10 @@
+### Chapter 5 - If Statements
+
+
+
+cars = ['ferrari','audi','bmw','toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
