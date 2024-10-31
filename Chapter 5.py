145f88c4893e8b44893efc255769fa21f4e8f6c3
Kamish-69 <kamish7569@gmail.com>
2024-10-31 19:06:08 +0530

    .

--- Chapter 5.py
+++ Chapter 5.py
@@ -1,10 +1,18 @@
 ### Chapter 5 - If Statements
 
-
-
+# A Simple Example
 cars = ['ferrari','audi','bmw','toyota']
 for car in cars:
     if car == 'bmw':
         print(car.upper())
     else:
         print(car.title())
+
+# Checking for Inequality
+requested_topping = 'Pineapple'
+served_topping = 'pepperoni'
+
+print(f"\nWaiter - 'Here is your ordered pizza with {served_topping}.'")
+if served_topping.lower() != requested_topping.lower():
+    print(f"Customer - 'But I wanted {requested_topping.lower()} topping.'")
+
